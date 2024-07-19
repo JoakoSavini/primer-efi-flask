cf73a00b775a7ff84b0acac27b7431c608a2b21b
tomighersi <tomigh717@gmail.com>
2024-07-19 08:48:03 -0300

    Version Anterior
    Volvi de version, por problemas en la base de datos e hice el funcionamiento en template para mostrar marcas, modelos y los celulares con marca y modelo, con un form para agregar marcas, modelos y celulares

--- app.py
+++ app.py
@@ -27,11 +27,57 @@ def index():
 def categorias():
     return render_template('categorias.html')
 
-@app.route('/marcas')
+@app.route('/marcas', methods=['GET', 'POST'])
 def marcas():
-    return render_template('marcas.html')
+    marcas = Marca.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nueva_marca = Marca(nombre=nombre)
+        db.session.add(nueva_marca)
+        db.session.commit()
+        return redirect(url_for('marcas'))
+    return render_template('marcas.html', marcas=marcas)
 
 @app.route('/precios')
 def precios():
     return render_template('precios.html')
+
+@app.route('/modelos', methods=['GET', 'POST'])
+def modelos():
+    modelos = Modelo.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nuevo_modelo = Modelo(nombre=nombre)
+        db.session.add(nuevo_modelo)
+        db.session.commit()
+        return redirect(url_for('modelos'))
+    return render_template('modelos.html', modelos=modelos)
+
+@app.route('/celulares', methods=['POST', 'GET'])
+def celulares():
+    celulares = Celular.query.all()
+    marcas = Marca.query.all()
+    modelos = Modelo.query.all()
+
+    if request.method == 'POST':
+        modelo = request.form['modelo']
+        marca = request.form['marca']
+        celular_nuevo = Celular(
+            modelo_id=modelo,
+            marca_id=marca,
+        )
+        db.session.add(celular_nuevo)
+        db.session.commit()
+        return redirect(url_for('celulares'))
+
+    return render_template(
+        'celulares.html',
+        celulares=celulares,
+        marcas=marcas,
+        modelos=modelos,
+    )
+
+
 
