405e5b0a3b7c37b55f8e557216f31d54c0041675
SalvadorCoco <salvadorcocosanchez@gmail.com>
2024-10-28 18:26:22 -0300

    Schemas y views comentadas

--- schemas.py
+++ schemas.py
@@ -1,118 +1,118 @@
-from app import ma
-from marshmallow import validates, ValidationError
-from models import User, Categoria, Celular, Especificacion, Fabricante, Gama, Marca, Modelo, Proveedor, SistemaOperativo
+# from app import ma
+# from marshmallow import validates, ValidationError
+# from models import User, Categoria, Celular, Especificacion, Fabricante, Gama, Marca, Modelo, Proveedor, SistemaOperativo
 
-class UserSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = User
+# class UserSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = User
         
-    id = ma.auto_field()
-    username = ma.auto_field()
-    password_hash = ma.auto_field()
-    is_admin = ma.auto_field()
+#     id = ma.auto_field()
+#     username = ma.auto_field()
+#     password_hash = ma.auto_field()
+#     is_admin = ma.auto_field()
     
-    @validates('username')
-    def validate_username(self, value):
-        user = User.query.filter_by(username=value).first()
-        if user:
-            raise ValidationError("Ya existe un usuario con ese nombre")
-        return value
+#     @validates('username')
+#     def validate_username(self, value):
+#         user = User.query.filter_by(username=value).first()
+#         if user:
+#             raise ValidationError("Ya existe un usuario con ese nombre")
+#         return value
 
     
-class UserMinimalSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = User
+# class UserMinimalSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = User
         
-    username = ma.auto_field()
+#     username = ma.auto_field()
   
-class MarcaSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Marca
+# class MarcaSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = Marca
         
-    id = ma.auto_field()
-    nombre = ma.auto_field()
+#     id = ma.auto_field()
+#     nombre = ma.auto_field()
     
     
-class ModeloSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Modelo
+# class ModeloSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = Modelo
         
-    id = ma.auto_field()
-    nombre = ma.auto_field()
-    marca_id = ma.auto_field()
-    marca = ma.Nested(MarcaSchema)  
+#     id = ma.auto_field()
+#     nombre = ma.auto_field()
+#     marca_id = ma.auto_field()
+#     marca = ma.Nested(MarcaSchema)  
   
-class CelularSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Celular
+# class CelularSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = Celular
         
-    id = ma.auto_field()
-    nombre = ma.auto_field()
-    usado = ma.auto_field()
-    precio = ma.auto_field()
-    proveedor_id = ma.auto_field()
-    especificacion_id = ma.auto_field()
-    modelo_id = ma.auto_field()
-    categoria_id = ma.auto_field()
-    gama_id = ma.auto_field()
-    sistema_operativo_id = ma.auto_field()
+#     id = ma.auto_field()
+#     nombre = ma.auto_field()
+#     usado = ma.auto_field()
+#     precio = ma.auto_field()
+#     proveedor_id = ma.auto_field()
+#     especificacion_id = ma.auto_field()
+#     modelo_id = ma.auto_field()
+#     categoria_id = ma.auto_field()
+#     gama_id = ma.auto_field()
+#     sistema_operativo_id = ma.auto_field()
     
-    modelo = ma.Nested(ModeloSchema)
+#     modelo = ma.Nested(ModeloSchema)
     
 
-class CategoriaSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Categoria
+# class CategoriaSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = Categoria
 
-    id = ma.auto_field()
-    nombre = ma.auto_field()
-    celular = ma.Nested(CelularSchema)
+#     id = ma.auto_field()
+#     nombre = ma.auto_field()
+#     celular = ma.Nested(CelularSchema)
 
 
-class EspecificacionSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Especificacion
+# class EspecificacionSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = Especificacion
     
-    id = ma.auto_field()
-    ram = ma.auto_field()
-    almacenamiento = ma.auto_field()
-    celular =  ma.Nested(CelularSchema)
+#     id = ma.auto_field()
+#     ram = ma.auto_field()
+#     almacenamiento = ma.auto_field()
+#     celular =  ma.Nested(CelularSchema)
 
 
-class ProveedorSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Proveedor
-    id = ma.auto_field() 
-    nombre = ma.auto_field()
-    localidad = ma.auto_field()
-    contacto = ma.auto_field()
-    celular =  ma.Nested(CelularSchema)
-    fabricante_id = ma.auto_field()
+# class ProveedorSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = Proveedor
+#     id = ma.auto_field() 
+#     nombre = ma.auto_field()
+#     localidad = ma.auto_field()
+#     contacto = ma.auto_field()
+#     celular =  ma.Nested(CelularSchema)
+#     fabricante_id = ma.auto_field()
 
 
-class FabricanteSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Fabricante
+# class FabricanteSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = Fabricante
 
-    id = ma.auto_field() 
-    nombre = ma.auto_field()
-    localidad = ma.auto_field()
-    contacto = ma.auto_field()
+#     id = ma.auto_field() 
+#     nombre = ma.auto_field()
+#     localidad = ma.auto_field()
+#     contacto = ma.auto_field()
     
-    proveedor = ma.Nested(ProveedorSchema)
+#     proveedor = ma.Nested(ProveedorSchema)
 
-class GamaSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Gama
+# class GamaSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = Gama
     
-    id = ma.auto_field()
-    nombre = ma.auto_field()
-    celular = ma.Nested(CelularSchema)
+#     id = ma.auto_field()
+#     nombre = ma.auto_field()
+#     celular = ma.Nested(CelularSchema)
 
-class SistemaOperativoSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = SistemaOperativo
+# class SistemaOperativoSchema(ma.SQLAlchemySchema):
+#     class Meta:
+#         model = SistemaOperativo
     
-    id = ma.auto_field()
-    nombre = ma.auto_field()
-    celular = ma.Nested(CelularSchema)
+#     id = ma.auto_field()
+#     nombre = ma.auto_field()
+#     celular = ma.Nested(CelularSchema)
--- views/celular_views.py
+++ views/celular_views.py
@@ -1,58 +1,58 @@
-from flask import Blueprint, request, jsonify, make_response
+# from flask import Blueprint, request, jsonify, make_response
 
-from app import db
+# from app import db
 
-from models import Celular, Marca, Modelo, Categoria, Especificacion, Proveedor, Fabricante, Gama, SistemaOperativo
+# from models import Celular, Marca, Modelo, Categoria, Especificacion, Proveedor, Fabricante, Gama, SistemaOperativo
 
-from schemas import MarcaSchema, ModeloSchema, CelularSchema, CategoriaSchema, EspecificacionSchema, ProveedorSchema, FabricanteSchema, GamaSchema, SistemaOperativoSchema
+# from schemas import MarcaSchema, ModeloSchema, CelularSchema, CategoriaSchema, EspecificacionSchema, ProveedorSchema, FabricanteSchema, GamaSchema, SistemaOperativoSchema
 
-celular_bp = Blueprint('celulares', __name__)
+# celular_bp = Blueprint('celulares', __name__)
 
-@celular_bp.route('/marcas', methods=['GET'])
-def marcas():
-    marcas = Marca.query.all()
-    return MarcaSchema().dump(marcas, many=True)
+# @celular_bp.route('/marcas', methods=['GET'])
+# def marcas():
+#     marcas = Marca.query.all()
+#     return MarcaSchema().dump(marcas, many=True)
 
-@celular_bp.route('/modelos', methods=['GET'])
-def modelos():
-    modelos = Modelo.query.all()
-    return ModeloSchema().dump(modelos, many=True)
+# @celular_bp.route('/modelos', methods=['GET'])
+# def modelos():
+#     modelos = Modelo.query.all()
+#     return ModeloSchema().dump(modelos, many=True)
 
-@celular_bp.route('/celulares', methods=['GET'])
-def celulares():
-    celulares = Celular.query.all()
-    return CelularSchema().dump(celulares, many=True)
-
-
-
-@celular_bp.route('/categorias', methods=['GET'])
-def categoria():
-    categoria = Categoria.query.all()
-    return CategoriaSchema().dump(categoria, many=True)
+# @celular_bp.route('/celulares', methods=['GET'])
+# def celulares():
+#     celulares = Celular.query.all()
+#     return CelularSchema().dump(celulares, many=True)
 
 
-@celular_bp.route('/especificaciones', methods=['GET'])
-def especificacion():
-    especificacion = Especificacion.query.all()
-    return EspecificacionSchema().dump(especificacion, many=True)
 
-@celular_bp.route('/proveedores', methods=['GET'])
-def proveedor():
-    proveedor = Proveedor.query.all()
-    return ProveedorSchema().dump(proveedor, many=True)
+# @celular_bp.route('/categorias', methods=['GET'])
+# def categoria():
+#     categoria = Categoria.query.all()
+#     return CategoriaSchema().dump(categoria, many=True)
 
 
-@celular_bp.route('/fabricantes', methods=['GET'])
-def fabricante():
-    fabricante = Fabricante.query.all()
-    return FabricanteSchema().dump(fabricante, many=True)
+# @celular_bp.route('/especificaciones', methods=['GET'])
+# def especificacion():
+#     especificacion = Especificacion.query.all()
+#     return EspecificacionSchema().dump(especificacion, many=True)
 
-@celular_bp.route('/gamas', methods=['GET'])
-def gama():
-    gama = Gama.query.all()
-    return GamaSchema().dump(gama, many=True)
+# @celular_bp.route('/proveedores', methods=['GET'])
+# def proveedor():
+#     proveedor = Proveedor.query.all()
+#     return ProveedorSchema().dump(proveedor, many=True)
 
-@celular_bp.route('/sistemasOperativos', methods=['GET'])
-def sistemaOperativo():
-    sistemaOperativo = SistemaOperativo.query.all()
-    return SistemaOperativoSchema().dump(sistemaOperativo, many=True)
+
+# @celular_bp.route('/fabricantes', methods=['GET'])
+# def fabricante():
+#     fabricante = Fabricante.query.all()
+#     return FabricanteSchema().dump(fabricante, many=True)
+
+# @celular_bp.route('/gamas', methods=['GET'])
+# def gama():
+#     gama = Gama.query.all()
+#     return GamaSchema().dump(gama, many=True)
+
+# @celular_bp.route('/sistemasOperativos', methods=['GET'])
+# def sistemaOperativo():
+#     sistemaOperativo = SistemaOperativo.query.all()
+#     return SistemaOperativoSchema().dump(sistemaOperativo, many=True)
